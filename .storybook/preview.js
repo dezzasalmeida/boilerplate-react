3fe854163af894ec4afc53d98c973349eb1bb623
dezzasalmeida <dezzasalmeida@gmail.com>
2021-04-19 15:38:33 -0300

    feat: absolute path config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../src/styles/global';
-import theme from '../src/styles/theme';
+import theme from 'styles/theme';
 
 const decorators = [
   (Story) => (
