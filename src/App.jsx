8c2c3fc3d9e80505d64e811fb2ba5620e17ae214
dezzasalmeida <dezzasalmeida@gmail.com>
2021-04-19 13:07:30 -0300

    feat: initial settings

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './components/Main';
+import './styles/global.css';
+
+const App = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
+
+export default App;
