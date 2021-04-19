f8ae2eacd6fa1d00c2a5bae16b95bea75e71c3df
dezzasalmeida <dezzasalmeida@gmail.com>
2021-04-19 15:37:49 -0300

    feat: theme provider config to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,13 @@
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../src/styles/global';
+import theme from '../src/styles/theme';
 
 const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ];
 
