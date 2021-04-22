49d140cf21bd82b87dc63d35f8cf1ac7010c8fa6
dezzasalmeida <dezzasalmeida@gmail.com>
2021-04-22 16:04:58 -0300

    feat: add button example

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Button from '.';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta;
+
+export const Default: Story = (args) => <Button {...args} />;
+
+Default.args = {
+  children: 'Buy now'
+};
